317545735ee49200a3a3524d89f2e9bd2d0288b3
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-15 20:50:29 +0200

    debug

--- removed_profils.py
+++ removed_profils.py
@@ -1,7 +1,7 @@
 from config import *
 from utils import reformat_dataset,print_loading_bar
 
-merged_df = df_old[[PROFILES_COL]].merge(df_new[[PROFILES_COL]], on=PROFILES_COL, how='left')
+merged_df = df_old[[PROFILES_COL]].merge(df_new[[PROFILES_COL]], on=PROFILES_COL, how='left',indicator=True)
 removed_ids = merged_df.loc[merged_df._merge=="left_only",PROFILES_COL].drop_duplicates()
 
 # format the dataframe the good way
