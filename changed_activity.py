5c4cc96bd1c41969109531124a3fdf85bc919c57
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-15 21:42:05 +0200

    debug

--- changed_activity.py
+++ changed_activity.py
@@ -5,6 +5,8 @@ columns_of_interest = [PROFILES_COL,COL_CHANGE_ACTIVITY]
 #drop line if both old and new activity are NULL
 merged_df = df_old[columns_of_interest].merge(df_new[columns_of_interest], on=PROFILES_COL, suffixes=('_old', '_new'), how='inner').dropna(subset=[COL_CHANGE_ACTIVITY+"_old",COL_CHANGE_ACTIVITY+"_new"], thresh=1)
 
+
+print(len(merged_df))
 changed_samples_ids = merged_df.loc[(merged_df[COL_CHANGE_ACTIVITY+'_old'] != merged_df[COL_CHANGE_ACTIVITY+'_new']),PROFILES_COL].unique()
 
 df_changed_activity_new = df_new[df_new[PROFILES_COL].isin(changed_samples_ids)]
