e404fbbc1cfae829d1a208fea6823c2a40c11209
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-16 11:17:24 +0200

    debufg

--- config.py
+++ config.py
@@ -12,19 +12,6 @@ args = parser.parse_args()
 
 
 
-def read_files(args):
-    if args.ftype =="csv":
-        df_old = pd.read_csv(args.old_path, sep="|", low_memory=False)
-        df_new = pd.read_csv(args.new_path, sep="|", low_memory=False)
-    elif args.ftype =="parquet":
-        df_old = pd.read_parquet(args.old_path)
-        df_new = pd.read_parquet(args.new_path)
-    else:
-        print("Wrong ftype argument")
-        exit(1)
-    return df_old,df_new
-
-
 COL_CHANGE_ACTIVITY = "Code secteur d'activité"
 PROFILES_COL = "Identification nationale PP"
 COL_CHANGE_NUMBER_VOIE = "Numéro Voie (coord. structure)"
@@ -47,12 +34,26 @@ address_cols = [
 columns_of_interest =["Identification nationale PP", "Code secteur d'activité",'Nom d\'exercice',"Prénom d\'exercice",'Libellé profession'\
                        ,'Libellé savoir-faire','Code catégorie professionnelle'] + address_cols
 
-df_old = df_old[columns_of_interest].drop_duplicates()
-df_new = df_new[columns_of_interest].drop_duplicates()
+
+def read_files(args):
+    if args.ftype =="csv":
+        df_old = pd.read_csv(args.old_path, sep="|", low_memory=False)
+        df_new = pd.read_csv(args.new_path, sep="|", low_memory=False)
+    elif args.ftype =="parquet":
+        df_old = pd.read_parquet(args.old_path)
+        df_new = pd.read_parquet(args.new_path)
+    else:
+        print("Wrong ftype argument")
+        exit(1)
+
+    df_old = df_old[columns_of_interest].drop_duplicates()
+    df_new = df_new[columns_of_interest].drop_duplicates()
 
 
-#convert to string then strip(), to avoid (having categories like "A " and "A")
-for col in df_old.columns:
-  if df_old[col].dtype=="object":
-    df_old[col] = df_old[col].astype("str").apply(lambda x:x.strip())
-    df_new[col] = df_new[col].astype("str").apply(lambda x:x.strip())
+    #convert to string then strip(), to avoid (having categories like "A " and "A")
+    for col in df_old.columns:
+        if df_old[col].dtype=="object":
+            df_old[col] = df_old[col].astype("str").apply(lambda x:x.strip())
+            df_new[col] = df_new[col].astype("str").apply(lambda x:x.strip())
+
+    return df_old,df_new
