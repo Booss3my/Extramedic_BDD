925e76364b5bed1aa8157aff4257d4cecaf43df0
Bastamy Oussama <oussama.bastamy@bordeaux-inp.fr>
2023-10-16 10:56:08 +0200

    chna&ged_loc debug

--- changed_location.py
+++ changed_location.py
@@ -6,40 +6,41 @@ def concatenate_values(row):
     concatenated_values = ' - '.join(str(row[col]) if pd.notna(row[col]) else '' for col in address_cols)
     return concatenated_values
 
-df_old = df_old.dropna(subset=address_cols)
-df_new = df_new.dropna(subset=address_cols)
+df_old= df_old.drop_duplicates(subset=PROFILES_COL,keep="first")
+df_new= df_new.drop_duplicates(subset=PROFILES_COL,keep="last")
 
-merged_df = df_old.merge(df_new, on=PROFILES_COL, suffixes=('_old', '_new'),how="inner")
 
-changes_mask = (
-    (merged_df[COL_CHANGE_NUMBER_VOIE + '_old'] != merged_df[COL_CHANGE_NUMBER_VOIE + '_new']) |
-    (merged_df[COL_CHANGE_TYPE_VOIE + '_old'] != merged_df[COL_CHANGE_TYPE_VOIE + '_new']) |
-    (merged_df[COL_CHANGE_LIBELLE_VOIE + '_old'] != merged_df[COL_CHANGE_LIBELLE_VOIE + '_new']) |
-    (merged_df[COL_CHANGE_MENTION + '_old'] != merged_df[COL_CHANGE_MENTION + '_new']) |
-    (merged_df[COL_CHANGE_CEDEX + '_old'] != merged_df[COL_CHANGE_CEDEX + '_new']) |
-    (merged_df[COL_CHANGE_CODE_POSTAL + '_old'] != merged_df[COL_CHANGE_CODE_POSTAL + '_new']) |
-    (merged_df[COL_CHANGE_CODE_COMMUNE + '_old'] != merged_df[COL_CHANGE_CODE_COMMUNE + '_new']) |
-    (merged_df[COL_CHANGE_CODE_PAYS + '_old'] != merged_df[COL_CHANGE_CODE_PAYS + '_new'])
-)
+for i,col in enumerate(address_cols):
+  if i==0:
+    old_add = df_old[col].fillna("").copy().astype("str")
+    new_add = df_new[col].fillna("").copy().astype("str")
+  else:
+    new_add+= " " + df_new[col].fillna("").astype("str")
+    old_add+= " " + df_old[col].fillna("").astype("str")
+
+df_old.drop(address_cols,axis=1,inplace=True)
+df_new.drop(address_cols,axis=1,inplace=True)
+
+df_old["full_address"] = old_add
+df_new["full_address"] = new_add
+
+merged_df = df_old[[PROFILES_COL,"full_address"]].merge(df_new, on=PROFILES_COL, suffixes=('_old', '_new'),how="inner")
+
+
+changes_mask = (merged_df["full_address_old"] != merged_df["full_address_new"])
 
 changes_df = merged_df[changes_mask].drop_duplicates()
 
 
-changes_df.merge()
-address_changed_ids = changes_df['Identification nationale PP'].unique()
-
-df_new_address_changed = df_new[df_new[PROFILES_COL].isin(address_changed_ids)]
-df_old_address_changed = df_old[df_old[PROFILES_COL].isin(address_changed_ids)]
-
 # format the dataframe the good way
 address_df = pd.DataFrame()
-address_df['Nom_complet'] = df_new_address_changed['Nom d\'exercice'] + ' ' + df_new_address_changed["Prénom d\'exercice"]
-address_df['Numéro_identification'] = df_new_address_changed['Identification nationale PP']
-address_df['Profession'] = df_new_address_changed['Libellé profession']
-address_df['Spécialité'] = df_new_address_changed['Libellé savoir-faire']
-address_df['Type_d\'exercice'] = df_new_address_changed['Code catégorie professionnelle']
-address_df['Ancienne_adresse'] = df_old_address_changed.apply(concatenate_values, axis=1).tolist()
-address_df["Nouvelle_adresse"] = df_new_address_changed.apply(concatenate_values, axis=1)
+address_df['Nom_complet'] = changes_df['Nom d\'exercice'] + ' ' + changes_df["Prénom d\'exercice"]
+address_df['Numéro_identification'] = changes_df['Identification nationale PP']
+address_df['Profession'] = changes_df['Libellé profession']
+address_df['Spécialité'] = changes_df['Libellé savoir-faire']
+address_df['Type_d\'exercice'] = changes_df['Code catégorie professionnelle']
+address_df['Ancienne_adresse'] = changes_df["full_address_old"]
+address_df["Nouvelle_adresse"] = changes_df["full_address_new"]
 
 # export the dataframe
 address_df.to_csv("changement_adresse.csv", index=False)
